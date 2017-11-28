bb7f29378487d9be069a6603b2f8911b47cd1463
Julee117 <juliannne@gmail.com>
2017-11-27 23:19:18 -0500

    Took out instance method

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,10 +1,4 @@
 module Slugifiable
-  module InstanceMethods
-    def slug
-      self.name.downcase.gsub(" ", "-")
-    end
-  end
-
   module ClassMethods
     def find_by_slug(slug)
       self.all.find { |a| a.slug == slug }
