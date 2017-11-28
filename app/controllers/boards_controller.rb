28aa35ae1ad86a552b307a7000ef2989469fe4ba
Julee117 <juliannne@gmail.com>
2017-11-28 00:09:46 -0500

    Create patch, edit methods

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -24,4 +24,36 @@ class BoardsController < ApplicationController
       redirect to "/boards/#{@board.slug}"
     end
   end
+
+  get '/boards/:slug' do
+    @board = Board.find_by_slug(params[:slug])
+    if logged_in?
+      erb :"/boards/show_board"
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/boards/:slug/edit' do
+    if !logged_in?
+      redirect to '/login'
+    else
+      @board = Board.find_by_slug(params[:slug])
+      if @board.user_id == current_user.id
+        erb :"/boards/edit_board"
+      else
+        redirect to "/boards"
+      end
+    end
+  end
+
+  patch '/boards/:slug' do
+    @board = current_user.boards.find_by_slug(params[:slug])
+    if params[:name].empty?
+      redirect to "/boards/#{@board.slug}/edit"
+    else
+      @board.update(name: params[:name])
+      redirect to "/boards/#{@board.slug}"
+    end
+  end
 end
