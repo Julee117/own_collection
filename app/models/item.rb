6ca9fd9f2fbfb896258c83260ee7cbe741e10c18
github username <github email address>
2017-12-06 12:28:47 -0500

    Update models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ActiveRecord::Base
   belongs_to :board
+  has_many :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
-  validates_presence_of :username, :email, :password
+  validates :username, :email, presence: true
+  validates :username, uniqueness: true
 
   has_many :boards
   has_many :items, through: :boards
+  has_many :reviews
   has_secure_password
 
   extend Slugifiable::ClassMethods
 
   def slug
     self.username.downcase.gsub(" ", "-")
-  end  
+  end
 end
