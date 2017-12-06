e395ff341fb6df61352d468c1a45910efd3dd78e
github username <github email address>
2017-12-06 13:09:06 -0500

    Add post '/items/:item_id/reviews', get '/reviews/:id'

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+  post '/items/:item_id/reviews' do
+    @item = Item.find_by(params[:item_id])
+    @review = @item.reviews.build(params[:review])
+    @review.user = current_user
+    if @review.save
+      flash[:message] = "Review was submitted!"
+      redirect to "/reviews/#{@item.id}"
+    else
+      flash[:message] = "Review failed!"
+      redirect to "/items/#{@item.id}"
+    end
+
+  end
+
+  get '/reviews/:id' do
+    @item = Item.find_by_id(params[:id])
+    @review = Review.find_by(params[:item_id])
+    erb :'reviews/show_item_review'
+  end
+end
